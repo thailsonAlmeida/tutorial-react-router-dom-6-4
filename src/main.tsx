e3c4648cd4522a7a0364db4d43094f364bc681c0
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-10-16 13:56:39 -0300

    Index Routes

--- src/main.tsx
+++ src/main.tsx
@@ -5,18 +5,24 @@ import Expenses from "./routes/Expenses/index.tsx";
 import Invoices from "./routes/Invoices/index.tsx";
 import NotFound from "./routes/NotFound/index.tsx";
 import Invoice from "./routes/Invoices/Invoice/index.tsx";
+import InvoiceIndex from "./routes/Invoices/InvoiceIndex/index.tsx";
+import Welcome from "./routes/Welcome/index.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
-    <Routes>
+    <Routes>      
       <Route path="/" element={<App />}>
+        <Route index element={ <Welcome /> } />
+
         <Route path="expenses" element={<Expenses />} />
 
-        <Route path="invoices" element={<Invoices />}> 
+        <Route path="invoices" element={<Invoices />}>
+          <Route index element={ <InvoiceIndex /> } />
           <Route path=":invoiceId" element={<Invoice />} />
         </Route>
 
-        <Route path="*" element={ <NotFound /> }/>
+        <Route path="*" element={<NotFound />} />
+
       </Route>
     </Routes>
   </BrowserRouter>
---
+++ src/routes/Welcome/index.tsx
@@ -0,0 +1,7 @@
+export default function Welcome() {
+  return (
+    <main style={{ padding: "1rem" }}>
+      <h1>Seja bem vindo</h1>
+    </main>
+  );
+}

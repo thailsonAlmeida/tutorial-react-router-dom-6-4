8ea9a27cd79efdca05a3c5d197636110265e54d3
Thailson Almeida <103151779+thailsonAlmeida@users.noreply.github.com>
2023-10-16 11:46:34 -0300

    Adding a No Match Route

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Expenses from './routes/Expenses/index.tsx'
-import Invoices from './routes/Invoices/index.tsx'
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Expenses from "./routes/Expenses/index.tsx";
+import Invoices from "./routes/Invoices/index.tsx";
+import NotFound from "./routes/NotFound/index.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />} >
+      <Route path="/" element={<App />}>
         <Route path="expenses" element={<Expenses />} />
         <Route path="invoices" element={<Invoices />} />
+        <Route path="*" element={ <NotFound /> }/>
       </Route>
     </Routes>
-  </BrowserRouter>,
-)
+  </BrowserRouter>
+);
